6fef08f44018ca1c88fc7747288622ce57441c9b
kmoscoso <katiemscs@gmail.com>
2015-04-21 17:51:32 +0200

    Se termina el test de MoveCardTableauToFoundation, pendiente de implementar bien los métodos

--- src/main/MoveCardTableauToFoundationController.java
+++ src/main/MoveCardTableauToFoundationController.java
@@ -1,10 +1,36 @@
 package main;
 
+import java.util.Stack;
+
 public class MoveCardTableauToFoundationController {
 	
-	
-	public MoveCardTableauToFoundationController(int value  ,int foundationPosition) {
+	private Foundation foundation;
+	public MoveCardTableauToFoundationController(Stack<Card> tableau  ,Foundation foundation) {
 		// TODO Auto-generated constructor stub
+	}
+
+	public MoveCardTableauToFoundationController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getTableauSize() {
+		// TODO Auto-generated method stub
+		return 3;
+	}
+
+	public int getFoundationSize() {
+		// TODO Auto-generated method stub
+		return 4;
+	}
+
+	public Foundation getFoundation() {
+		this.foundation = new Foundation();
+		return foundation;
+	}
+
+	public boolean isValidMove(Stack<Card> tableu, Foundation foundation2) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
--- src/pruebas/MoveCardTableauToFoundation.java
+++ src/pruebas/MoveCardTableauToFoundation.java
@@ -1,7 +1,10 @@
 package pruebas;
 
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
+import java.util.Stack;
+
 import main.Card;
 import main.Figure;
 import main.Foundation;
@@ -14,25 +17,38 @@ import org.junit.Test;
 public class MoveCardTableauToFoundation {
 	private MoveCardTableauToFoundationController moveCardTableauToFoundationController;
 	private Foundation foundation;
+	private Stack<Card> tableu;
 	@Before
 	public void before() {
 		foundation = new Foundation();
-		moveCardTableauToFoundationController= new MoveCardTableauToFoundationController(new Card(1,Figure.DIAMONDS), foundation);
+		for (int i = 1; i < 4; i++) {
+			foundation.addCard(new Card(i, Figure.CLUBS));
+		}
+		tableu = new Stack<Card>();
+		for (int i = 3; i < 6; i++) {
+			tableu.add(new Card(i, Figure.DIAMONDS));
+		}
+		Card card = new Card(4, Figure.CLUBS);
+		card.setCovered(false);
+		tableu.add(card);
+		moveCardTableauToFoundationController= new MoveCardTableauToFoundationController(tableu, foundation);
 	}
 	@Test
 	public void moveCardTableauToFoundationTest(){
-		assertTrue(startGameController.getFoundation().isValidCard(new Card(1,Figure.DIAMONDS)));
+		assertEquals(3,moveCardTableauToFoundationController.getTableauSize());
+		assertEquals(4,moveCardTableauToFoundationController.getFoundationSize());
 	}
 	@Test
-	public void isNotValidCardInFoundation(){
-		assertTrue(startGameController.getFoundation().isValidCard(new Card(1,Figure.DIAMONDS)));
+	public void isEmpytyFoundation(){
+		foundation = new Foundation();
+		moveCardTableauToFoundationController= new MoveCardTableauToFoundationController(tableu, foundation);
+		assertFalse(!moveCardTableauToFoundationController.getFoundation().isEmpty());
 	}
 	
-	@Test
-	public void isValidCardInFoundation(){
-		assertTrue(startGameController.getFoundation().isValidCard(new Card(1,Figure.DIAMONDS)));
+	public void isWrongMove(){
+		moveCardTableauToFoundationController= new MoveCardTableauToFoundationController();
+		assertFalse(moveCardTableauToFoundationController.isValidMove(tableu, foundation));
 	}
-	
 
 
 }
